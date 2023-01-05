ef1ada697a92aecccd68ebb0796b864415906e64
Alok Singh <98412930+inquisitive9@users.noreply.github.com>
2023-01-05 18:16:31 +0530

    Cpp solution of Perfect Rectangle

---
+++ Microsoft/Q6PerfectRectangle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+ bool isRectangleCover(vector<vector<int>>& rectangles) {
+        map<pair<int, int>, int> mp;
+        for(vector<int>& x:rectangles){
+            mp[{x[0], x[1]}]++;
+            mp[{x[0], x[3]}]--;
+            mp[{x[2], x[1]}]--;
+            mp[{x[2], x[3]}]++;
+        }
+        int n_mark=0;
+        for(auto ptr=mp.begin();ptr!=mp.end();ptr++)
+            if(ptr->second!=0){
+                if(abs(ptr->second)!=1) return false;
+                n_mark++;
+            }
+        return n_mark==4;
+    }
+};
